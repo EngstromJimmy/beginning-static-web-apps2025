5eaa27fd207eaba86e6a316812b0dde4914cbaac
Jimmy Engström <jimmy@apeoholic.se>
2025-04-30 16:37:01 +0200

    Added blog posts

---
+++ Beginning Static Web Apps/Client/Services/BlogpostSummaryService.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+
+namespace Client.Services;
+public class BlogpostSummaryService(HttpClient httpClient)
+{
+    private readonly HttpClient _httpClient = httpClient;
+    public List<Blogpost>? Summaries;
+
+    public async Task LoadBlogpostSummaries()
+    {
+        if (Summaries == null)
+        {
+            Summaries = await _httpClient.GetFromJsonAsync<List<Blogpost>> ("api/blogposts");
+        }
+    }
+
+}
